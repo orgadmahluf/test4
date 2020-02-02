0a26f6b83978676ac66c98526892fd80e41c64db
orgad <orgad@svcollege.co.il>
2020-02-02 18:39:31 +0200

    Updates

---
+++ precache-manifest.7ae06e7612dc5b84d10cea751ccf7b1d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f139b3305f047f72049a91cf5abc6a3",
+    "url": "orgadmahluf.github.io/test4/index.html"
+  },
+  {
+    "revision": "2515d79cec8de975fb28",
+    "url": "orgadmahluf.github.io/test4/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "39e36a478adf71d8d8e9",
+    "url": "orgadmahluf.github.io/test4/static/css/main.efd71899.chunk.css"
+  },
+  {
+    "revision": "2515d79cec8de975fb28",
+    "url": "orgadmahluf.github.io/test4/static/js/2.e4279697.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "orgadmahluf.github.io/test4/static/js/2.e4279697.chunk.js.LICENSE"
+  },
+  {
+    "revision": "39e36a478adf71d8d8e9",
+    "url": "orgadmahluf.github.io/test4/static/js/main.81a2ce45.chunk.js"
+  },
+  {
+    "revision": "7b669e7afbb6d5ebd442",
+    "url": "orgadmahluf.github.io/test4/static/js/runtime-main.7f8bd2d4.js"
+  }
+]);
