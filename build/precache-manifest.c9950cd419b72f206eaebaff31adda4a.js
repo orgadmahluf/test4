0584f01fad45d5003d2c0cfbf676c42c188fd559
orgad <orgad@svcollege.co.il>
2020-02-02 18:33:55 +0200

    test

---
+++ build/precache-manifest.c9950cd419b72f206eaebaff31adda4a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8c096dac7bd040b92c063ba48e49be0",
+    "url": "shemSvcollege.github.io/TestSvcollege/index.html"
+  },
+  {
+    "revision": "1725b3844f6796f8c808",
+    "url": "shemSvcollege.github.io/TestSvcollege/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "b05e7ced720d288f3a0e",
+    "url": "shemSvcollege.github.io/TestSvcollege/static/css/main.0ce0b383.chunk.css"
+  },
+  {
+    "revision": "1725b3844f6796f8c808",
+    "url": "shemSvcollege.github.io/TestSvcollege/static/js/2.7cf32c17.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "shemSvcollege.github.io/TestSvcollege/static/js/2.7cf32c17.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b05e7ced720d288f3a0e",
+    "url": "shemSvcollege.github.io/TestSvcollege/static/js/main.c464015c.chunk.js"
+  },
+  {
+    "revision": "a0bf0309a73d2acec44d",
+    "url": "shemSvcollege.github.io/TestSvcollege/static/js/runtime-main.559b661c.js"
+  }
+]);
